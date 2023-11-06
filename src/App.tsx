fbb77133adec87b9b82d466fca500000927f9354
Allan Ramos <allangrds@gmail.com>
2023-11-06 13:54:31 -0300

    fix: changing input and button styles

--- src/App.tsx
+++ src/App.tsx
@@ -26,43 +26,45 @@ export const App = () => {
         <section>
           <h1 className="title">Clock</h1>
           <p className="timer">
-            {String(clockHours).padStart(2,'0')}:
+            {String(clockHours).padStart(2, '0')}:
             {String(clockMinutes).padStart(2, '0')}:
             {String(clockSeconds).padStart(2, '0')}
           </p>
         </section>
         <section>
           <h1 className="title">Timer</h1>
-          <p className={`timer ${isNegative ? 'negative': ''}`}>
+          <p className={`timer ${isNegative ? 'negative' : ''}`}>
             {isNegative && '-'}
-            {String(timerHours).padStart(2,'0')}:
+            {String(timerHours).padStart(2, '0')}:
             {String(timerMinutes).padStart(2, '0')}:
             {String(timerSeconds).padStart(2, '0')}
           </p>
-          <div className="columns-3">
-            <input
-              className="input"
-              type="number"
-              placeholder="Horas"
-              onChange={(e) => onChangeHour(parseInt(e.target.value || "0", 10))}
-            />
-            <input
-              className="input"
-              type="number"
-              placeholder="Minutos"
-              onChange={(e) => onChangeMinute(parseInt(e.target.value || "0", 10))}
-            />
-            <input
-              className="input"
-              type="number"
-              placeholder="Segundos"
-              onChange={(e) => onChangeSecond(parseInt(e.target.value || "0", 10))}
-            />
-          </div>
-          <div className="columns-3">
-            <button onClick={onStart} className="button">Iniciar</button>
-            <button onClick={onStop} className="button">Pausar</button>
-            <button onClick={onReset} className="button">Resetar</button>
+          <div className="columns-2">
+            <div className="rows-3">
+              <input
+                className="input"
+                type="number"
+                placeholder="Horas"
+                onChange={(e) => onChangeHour(parseInt(e.target.value || "0", 10))}
+              />
+              <input
+                className="input"
+                type="number"
+                placeholder="Minutos"
+                onChange={(e) => onChangeMinute(parseInt(e.target.value || "0", 10))}
+              />
+              <input
+                className="input"
+                type="number"
+                placeholder="Segundos"
+                onChange={(e) => onChangeSecond(parseInt(e.target.value || "0", 10))}
+              />
+            </div>
+            <div className="rows-3">
+              <button onClick={onStart} className="button">Iniciar</button>
+              <button onClick={onStop} className="button">Pausar</button>
+              <button onClick={onReset} className="button">Resetar</button>
+            </div>
           </div>
         </section>
       </div>
